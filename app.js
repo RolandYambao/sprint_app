53cccb885d8f3ca958ffe446ad5879d34bb5fe73
RolandYambao <rolandyambao@gmail.com>
2021-12-03 09:46:20 -0800

    Updated

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+const express = require('express');
+
 const { redundant } = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const layouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs'); // for our view (html like pages), we want to use 
+app.use(layouts);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.json({ message: 'Welcome to Spotify 2.0' });
+});
+
+app.listen(PORT, function () {
+    console.log(`Server running on PORT`, PORT);
+});
 
 // Implement CRUD
 
